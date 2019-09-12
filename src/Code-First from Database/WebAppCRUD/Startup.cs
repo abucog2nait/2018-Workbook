ed8afec70576160a40ad6afb0c1564d6994272be
abucog2nait <46579732+abucog2nait@users.noreply.github.com>
2019-09-12 14:51:12 -0600

    view products created

---
+++ src/Code-First from Database/WebAppCRUD/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppCRUD.Startup))]
+namespace WebAppCRUD
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ src/Code-First from Database/WestWindSystem/BLL/CRUDController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WebAppCRUD.BLL
+{
+    [DataObject]
+    public class CRUDController
+    {
+        #region Products CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Product> ListProducts()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        #endregion
+
+        #region Suppliers CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Supplier> ListSuppliers()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Suppliers.ToList();
+            }
+        }
+        #endregion
+
+        #region Categories CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Category> ListCategories()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+        #endregion
+
+        #region Addresses CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Address> ListAddresses()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Addresses.ToList();
+            }
+        }
+        #endregion
+
+    }
+
+}
