2901642a2e4247fda8cd8e920fc88191b90b8717
abucog2nait <46579732+abucog2nait@users.noreply.github.com>
2019-10-01 11:42:52 -0600

    updated

---
+++ src/Code-First from Database/WestWindSystem/POCOs/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.POCOs
+{
+
+    public class SupplierSumary
+    {
+        public string Company { get; set; }
+        public string Caegory { get; set; }
+        public string Phone { get; set; }
+        public IEnumerable<SupplierProduct> Products { get; set; }
+    }
+}
---
+++ src/Code-First from Database/WestWindSystem/POCOs/SupplierProduct.cs
@@ -0,0 +1,10 @@
+﻿namespace WestWindSystem.POCOs
+{
+    public class SupplierProduct
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string QtyPerUnit { get; set; }
+    }
+}
